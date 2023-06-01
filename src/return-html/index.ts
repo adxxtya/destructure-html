e5f4d4b8fdc2ce776ed2e3da630e3224d7d5e4b0
adxxtya <andronaut9@gmail.com>
2023-06-01 23:48:44 +0530

    Added getContentBetweenTags

--- src/return-html/index.ts
+++ src/return-html/index.ts
@@ -1,6 +1,11 @@
 import { findTagByClass, findTagByText } from "./Traverse";
+import { getContentBetweenTags } from "./Linear";
 
 export const Traverse = {
   findTagByClass,
   findTagByText,
+};
+
+export const Linear = {
+  getContentBetweenTags,
 };
